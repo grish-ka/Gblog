9f653df81b075c5bcf56c628c646bffc39d93175
grish-ka <grishka.krot@gmail.com>
2026-04-18 09:05:32 +0100

    fix config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'KEY'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-password'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -15,6 +15,6 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
-    RECAPTCHA_PUBLIC_KEY = 'KEY'
-    RECAPTCHA_PRIVATE_KEY = 'KEY'
+    ADMINS = ['grishka.krot@gmail.com']
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
